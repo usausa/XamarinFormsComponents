ac7d7371fc29d72745d54107979ac24b06f7ff96
usausa <machi.pon@gmail.com>
2020-07-05 16:57:37 +0900

    Update extensions

--- XamarinFormsComponents.Serializers/ResolverAdapterExtensions.cs
+++ XamarinFormsComponents.Serializers/ResolverAdapterExtensions.cs
@@ -1,21 +1,17 @@
 namespace XamarinFormsComponents
 {
     using System;
-
-    using Newtonsoft.Json;
-    using Newtonsoft.Json.Serialization;
+    using System.Text.Json;
 
     using XamarinFormsComponents.Serializers;
 
     public static class ResolverAdapterExtensions
     {
-        private static JsonSerializerSettings CreateDefaultSetting()
+        private static JsonSerializerOptions CreateDefaultOption()
         {
-            return new JsonSerializerSettings
+            return new JsonSerializerOptions
             {
-                ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                DateTimeZoneHandling = DateTimeZoneHandling.Utc,
-                DateFormatHandling = DateFormatHandling.IsoDateFormat
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase
             };
         }
 
@@ -25,11 +21,11 @@ namespace XamarinFormsComponents
             return adapter;
         }
 
-        public static IResolverAdapter AddJsonSerializer(this IResolverAdapter adapter, Action<JsonSerializerSettings> action)
+        public static IResolverAdapter AddJsonSerializer(this IResolverAdapter adapter, Action<JsonSerializerOptions> action)
         {
-            var settings = CreateDefaultSetting();
-            action(settings);
-            adapter.AddComponent<ISerializer>(new XamarinFormsComponents.Serializers.JsonSerializer(settings));
+            var option = CreateDefaultOption();
+            action(option);
+            adapter.AddComponent<ISerializer>(new XamarinFormsComponents.Serializers.JsonSerializer(option));
             return adapter;
         }
     }
