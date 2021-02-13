c7be0f692a4f9cb1fa7c8a25ede47be1f5bada2c
usausa <machi.pon@gmail.com>
2021-02-13 19:13:47 +0900

    Fix inspection

--- Example.FormsApp/Example.FormsApp.iOS/GlobalSuppressions.cs
+++ Example.FormsApp/Example.FormsApp.iOS/GlobalSuppressions.cs
@@ -1,1 +1,1 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:SA1300", Justification = "Ignore")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.NamingRules", "SA1300:ElementMustBeginWithUpperCaseLetter", Justification = "Ignore")]
--- Example.FormsApp/Example.FormsApp/GlobalSuppressions.cs
+++
@@ -1,2 +0,0 @@
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1710:IdentifiersShouldHaveCorrectSuffix", Justification = "Ignore")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Reliability", "CA2007:DoNotDirectlyAwaitATask", Justification = "Ignore")]
