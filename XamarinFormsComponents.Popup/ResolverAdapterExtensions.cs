98af3ff03731fc08b59798c01a734235879d1dce
usausa <machi.pon@gmail.com>
2021-06-02 16:56:05 +0900

    Update extensions

--- XamarinFormsComponents.Popup/ResolverAdapterExtensions.cs
+++ XamarinFormsComponents.Popup/ResolverAdapterExtensions.cs
@@ -10,5 +10,18 @@ namespace XamarinFormsComponents
             adapter.AddComponent<IPopupNavigator, PopupNavigator>();
             return adapter;
         }
+
+        public static IResolverAdapter UsePopupPageFactory<T>(this IResolverAdapter adapter)
+            where T : IPopupPageFactory
+        {
+            adapter.AddComponent<IPopupPageFactory, T>();
+            return adapter;
+        }
+
+        public static IResolverAdapter UsePopupPageFactory(this IResolverAdapter adapter, IPopupPageFactory popupPageFactory)
+        {
+            adapter.AddComponent(popupPageFactory);
+            return adapter;
+        }
     }
 }
